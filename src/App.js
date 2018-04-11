c9d410d8257d74f2506330fc0fd8f9b0b48753d8
Camilo Baquero <camilo.baquero@globant.com>
2018-04-10 19:02:04 -0500

    Basic List implementation on Index to load the Chats

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import { Navbar, Jumbotron, Button, ListGroup, ListGroupItem, Grid, Row } from 'react-bootstrap';
 import * as firebase from 'firebase';
 
 class App extends Component {
@@ -39,16 +39,37 @@ class App extends Component {
     return (
 
       <div className="App">
+        
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Stak</h1>
         </header>
+
+        <Grid>
+          
+          <Row>
+            <Jumbotron className="mx-auto" style={{ width: 500, margin: 20 }}>
+              <h1>Chats</h1>
+              <p>
+                Listado de los usuarios con conversaciones activas.
+              </p>
+
+              <ListGroup>
+                <ListGroupItem href="#link1">Link 1</ListGroupItem>
+              </ListGroup>
+
+              <p>
+                <Button bsStyle="primary">Learn more {this.state.mensajes}</Button>
+              </p>
+            </Jumbotron>
+          </Row>
+
+        </Grid>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-
-        <Button bsStyle="success">Hello {this.state.mensajes}</Button>
-
+        
       </div>
 
     );
